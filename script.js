9f6baf665b83eb1493d79bf89051f364cfd2c9d6
okayaparna <krisa304@newschool.edu>
2023-07-28 14:25:11 +0530

    grid-1

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-// Get the grid container element
-const gridContainer = document.querySelector('.grid-container');
+const gridContainer = document.getElementById('gridContainer');
 
-// Function to generate the grid
-function generateGrid(rows, cols) {
-    // Calculate total number of cells
-    const totalCells = rows * cols;
+// Function to create a grid item (unit) with event listeners for hover effect
+function createGridItem() {
+  const gridItem = document.createElement('div');
+  gridItem.className = 'grid-item';
+  
+  // Add event listener for hover effect
+  gridItem.addEventListener('mouseover', () => {
+    gridItem.style.backgroundColor = 'black';
+  });
 
-    // Create the grid cells dynamically
-    for (let i = 0; i < totalCells; i++) {
-        const gridUnit = document.createElement('div');
-        gridUnit.classList.add('grid-unit');
-        gridContainer.appendChild(gridUnit);
-    }
+  gridContainer.appendChild(gridItem);
 }
 
-// Call the function to generate a 100x100 grid
-generateGrid(100, 100);
+// Function to generate the grid with 100x100 units
+function generateGrid() {
+  for (let i = 0; i < 100 * 100; i++) {
+    createGridItem();
+  }
+}
+
+generateGrid();
