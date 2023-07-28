88fef4091ac4bba35a621251d2b74cefe62a20a6
okayaparna <krisa304@newschool.edu>
2023-07-28 15:36:25 +0530

    drag-draw

--- script.js
+++ script.js
@@ -1,23 +1,78 @@
-const gridContainer = document.getElementById('gridContainer');
+// const gridContainer = document.getElementById('gridContainer');
 
-// Function to create a grid item (unit) with event listeners for hover effect
-function createGridItem() {
-  const gridItem = document.createElement('div');
-  gridItem.className = 'grid-item';
+// // Function to create a grid item (unit) with event listeners for hover effect
+// function createGridItem() {
+//   const gridItem = document.createElement('div');
+//   gridItem.className = 'grid-item';
   
-  // Add event listener for hover effect
-  gridItem.addEventListener('mouseover', () => {
-    gridItem.style.backgroundColor = 'black';
-  });
+//   // Add event listener for hover effect
+//   gridItem.addEventListener('mouseover', () => {
+//     gridItem.style.backgroundColor = 'black';
 
-  gridContainer.appendChild(gridItem);
-}
+//   });
 
-// Function to generate the grid with 100x100 units
-function generateGrid() {
-  for (let i = 0; i < 100 * 100; i++) {
-    createGridItem();
-  }
-}
+//   gridContainer.appendChild(gridItem);
+// }
 
-generateGrid();
+// // Function to generate the grid with 100x100 units
+// function generateGrid() {
+//   for (let i = 0; i < 100 * 100; i++) {
+//     createGridItem();
+//   }
+// }
+
+// generateGrid();
+
+
+
+    const gridContainer = document.getElementById('gridContainer');
+    let isDragging = false;
+
+    // Function to create a grid item (unit)
+    function createGridItem() {
+      const gridItem = document.createElement('div');
+      gridItem.className = 'grid-item';
+
+      gridContainer.appendChild(gridItem);
+    }
+
+    // Function to generate the grid with 100x100 units
+    function generateGrid() {
+      for (let i = 0; i < 100 * 100; i++) {
+        createGridItem();
+      }
+    }
+
+    generateGrid();
+
+    // Event listeners for mouse/keyboard drag
+    gridContainer.addEventListener('mousedown', () => {
+      isDragging = true;
+    });
+
+    gridContainer.addEventListener('mouseup', () => {
+      isDragging = false;
+    });
+
+    gridContainer.addEventListener('mousemove', (event) => {
+      if (isDragging) {
+        // Find the grid item being hovered over
+        const gridItem = event.target;
+        
+        // Set the "dragging" class to the grid container
+        // This will change the background color to black for grid items while dragging
+        gridContainer.classList.add('dragging');
+        
+        // Check if the event target is a grid item to avoid changing the background color of the entire container
+        if (gridItem.classList.contains('grid-item')) {
+          gridItem.style.backgroundColor = 'black';
+        }
+      }
+    });
+
+    // Reset the dragging state and remove the "dragging" class when the mouse leaves the grid container
+    gridContainer.addEventListener('mouseleave', () => {
+      isDragging = false;
+      gridContainer.classList.remove('dragging');
+    });
+
